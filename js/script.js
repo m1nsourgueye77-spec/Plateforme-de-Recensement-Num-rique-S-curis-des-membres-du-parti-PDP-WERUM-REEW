5bf7ff783c53eeb4c90e4c07a0ef1974f96c7789
Africa Light Site <m1nsour.gueye77@gmail.com>
2026-09-06 00:21:34 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -136,3 +136,53 @@ function remplirFormulaire() {
     });
 
 }
+
+
+
+
+function ouvrirScanner() {
+
+    if (!CONFIG.fonctionsProfessionnelles.scannerQR) {
+
+        document.getElementById("messageScanner").innerHTML = `
+            🔒 <strong>Fonctionnalité professionnelle</strong><br><br>
+
+            Le scanner QR est disponible
+            dans la version professionnelle.
+
+            <br><br>
+
+            <button
+                type="button"
+                onclick="demanderActivation()"
+            >
+                📩 Demander l'activation
+            </button>
+        `;
+
+        return;
+    }
+
+    window.location.href = "scanner.html";
+}
+
+
+
+function demanderActivation() {
+
+    const message =
+        "Bonjour Mansour,\n\n" +
+        "Je souhaite demander l'activation " +
+        "de la version professionnelle " +
+        "de la plateforme PDP Wérum Réw.";
+
+    const telephone = "221776813749";
+
+    window.open(
+        "https://wa.me/" +
+        telephone +
+        "?text=" +
+        encodeURIComponent(message),
+        "_blank"
+    );
+}
